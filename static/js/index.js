ead88f8131dacf4834d275043e1034de01f296b0
SuperRoboBrain <66873890+Yuheng2000@users.noreply.github.com>
2026-01-10 23:59:54 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var options = {
+    var defaultOptions = {
         slidesToScroll: 1,
         slidesToShow: 1,
         loop: true,
@@ -8,8 +8,39 @@ $(document).ready(function() {
         autoplaySpeed: 3000,
     };
 
-    // Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+    // Initialize generic carousels (exclude our specific multi-item ones)
+    bulmaCarousel.attach('.carousel:not(.carousel-2-items):not(.carousel-3-items):not(.carousel-4-items)', defaultOptions);
 
+    // Initialize 2-item carousels
+    bulmaCarousel.attach('.carousel-2-items', {
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        pagination: true,
+    });
+
+    // Initialize 3-item carousels
+    bulmaCarousel.attach('.carousel-3-items', {
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        pagination: true,
+    });
+
+    // Initialize 4-item carousels (if needed later)
+    bulmaCarousel.attach('.carousel-4-items', {
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        pagination: true,
+    });
 })
-
